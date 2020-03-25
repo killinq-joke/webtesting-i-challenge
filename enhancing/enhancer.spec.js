d882e46878095ef2100c77ab7417f54ad71b96a3
Zak <zakadmaster@gmail.com>
2020-03-25 13:47:37 +0100

    Addition of repair tessts

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,29 @@
 const enhancer = require('./enhancer.js');
 // test away!
+const itemTest = {name: "thing", durability: 10, enhancement: 10}
+
+describe("enhancers", () => {
+    describe("repair", () => {
+        it("doesn't crash", () => {
+        enhancer.repair(itemTest)
+        })
+        it("repairs", () => {
+        expect(enhancer.repair(itemTest)).toEqual({ name: "thing", durability: 100, enhancement: 10 })
+        })
+    })
+    describe("succeed", () => {
+        it("doesn't crash", () => {
+        enhancer.succeed(itemTest)
+        })
+    })
+    describe("fail", () => {
+        it("doesn't crash", () => {
+        enhancer.fail(itemTest)
+        })
+    })
+    describe("get", () => {
+        it("doesn't crash", () => {
+        enhancer.get(itemTest)
+        })
+    })
+})
