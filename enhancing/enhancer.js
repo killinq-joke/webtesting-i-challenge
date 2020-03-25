41811607a8b7a7190a88e7fcc5f8e0a206bd3db6
Zak <zakadmaster@gmail.com>
2020-03-25 12:39:56 +0100

    initial commit

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,17 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement === 20) {
+    
+    return {...item}
+
+  } else {
+
+    return { ...item,
+             enhancement: item.enhancement + 1
+      };
+  }
+  
 }
 
 function fail(item) {
@@ -14,7 +24,9 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item,
+           durability: 100
+  };
 }
 
 function get(item) {
