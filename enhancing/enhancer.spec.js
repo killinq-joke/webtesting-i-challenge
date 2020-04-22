8fb298f4e434c6479af0b7fa90858ff99d93f42c
killinq-joke <zakadmaster@gmail.com>
2020-04-22 20:34:32 +0200

    Addition of repaire method & test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,22 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer", () => {
+  describe("repair()", () => {
+    it("takes the durability to 100", () => {
+      expect(enhancer.repair({ name: "thing", durability: 90 })).toEqual({
+        name: "thing",
+        durability: 100,
+      });
+    });
+  });
+  describe("succeed()", () => {
+    it("things", () => {});
+  });
+  describe("fail()", () => {
+    it("things", () => {});
+  });
+  describe("get()", () => {
+    it("things", () => {});
+  });
+});
