48e5f0c61dab0dde0a984863a8a38418d7d52dd3
killinq-joke <zakadmaster@gmail.com>
2020-04-22 20:46:45 +0200

    Addition of succeed method & tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -4,14 +4,30 @@ const enhancer = require("./enhancer.js");
 describe("enhancer", () => {
   describe("repair()", () => {
     it("takes the durability to 100", () => {
-      expect(enhancer.repair({ name: "thing", durability: 90 })).toEqual({
+      expect(
+        enhancer.repair({ name: "thing", durability: 90, enhancement: 10 })
+      ).toEqual({
         name: "thing",
         durability: 100,
+        enhancement: 10,
       });
     });
   });
   describe("succeed()", () => {
-    it("things", () => {});
+    it("increases enhancement by 1", () => {
+        expect(
+          enhancer.succeed({ name: "thing", durability: 90, enhancement: 10 })
+        ).toEqual({ name: "thing", durability: 90, enhancement: 11 });
+    });
+    it("doesn't increase enhancement if at 20", () => {
+        expect(
+          enhancer.succeed({ name: "thing", durability: 90, enhancement: 20 })
+        ).toEqual({
+          name: "thing",
+          durability: 90,
+          enhancement: 20,
+        });
+    })
   });
   describe("fail()", () => {
     it("things", () => {});
